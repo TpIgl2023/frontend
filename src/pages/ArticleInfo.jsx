13ff6b2ffac6b003d626a4c360538e0d7382729c
walaanedjam <w.nedjam@gmail.com>
2024-01-05 21:24:32 +0100

    21.24

--- src/pages/ArticleInfo.jsx
+++ src/pages/ArticleInfo.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavArticles from '../components/navbars/NavArticles';
 import FooterSigned from '../components/footers/FooterSigned';
 
 export default function ArticleInfo() {
-    const [selectedButton, setSelectedButton] = useState(null);
+    const [selectedButton, setSelectedButton] = useState('titre');
 
     const handleButtonClick = (buttonName) => {
         setSelectedButton(buttonName);
     };
+
+    useEffect(() => {
+        handleButtonClick('titre');
+    }, []);
+
+    const buttons = ['titre', 'resume', 'institutuin', 'auteurs', 'url', 'text', 'motcle'];
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -15,12 +21,12 @@ export default function ArticleInfo() {
 
             <div className="container mx-auto mt-10 border border-blue-500 rounded-lg overflow-hidden">
                 <div className="flex">
-                    {['titre', 'resume', 'institutuin', 'auteurs', 'url', 'text'].map((buttonName, index) => (
+                    {buttons.map((buttonName, index) => (
                         <button
                             key={index}
                             onClick={() => handleButtonClick(buttonName)}
                             className={`flex-1 px-4 py-2 border border-blue-500 ${selectedButton === buttonName ? 'bg-blue-500 text-white' : 'bg-white text-gray-700'
-                                } ${index === 0 ? 'rounded-l' : ''} ${index === 5 ? 'rounded-r' : ''} ${index !== 0 ? 'border-l' : ''
+                                } ${index === 0 ? 'rounded-l' : ''} ${index === buttons.length - 1 ? 'rounded-r' : ''} ${index !== 0 ? 'border-l' : ''
                                 }`}
                         >
                             {buttonName.charAt(0).toUpperCase() + buttonName.slice(1)}
@@ -28,7 +34,7 @@ export default function ArticleInfo() {
                     ))}
                 </div>
 
-                <div className="mt-2 p-4 ">
+                <div className="mt-2 p-4 border ">
                     {selectedButton && (
                         <div>
                             <h2 className="text-2xl font-bold mb-4">Contenu du bouton {selectedButton} :</h2>
