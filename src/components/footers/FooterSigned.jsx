f53aae590e7376c4d2eae357299c50b177140b97
ccd <lm_bouchene@esi.dz>
2024-01-10 09:56:17 -1100

    added responsiveness

--- src/components/footers/FooterSigned.jsx
+++ src/components/footers/FooterSigned.jsx
@@ -3,27 +3,35 @@ import { Link } from 'react-router-dom'
 
 export default function FooterSigned() {
   return (
-    <div className="h-[400px] w-full bg-[#1B76FF] flex flex-col justify-between">
+    <div className="h-[450px] w-full bg-[#1B76FF] flex flex-col justify-between text-white">
       <div className="flex justify-evenly py-16">
-        <div className="flex flex-col gap-8 w-[400px]">
-            <h1 className="font-jost text-5xl text-white">Logo</h1>
-            <p className="font-inter text-white text-xl">Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, </p>
+        <div className="hidden md:block">
+          <div className="flex flex-col gap-8 w-[250px] lg:w-[400px] ">
+              <h1 className="font-jost text-3xl lg:text-5xl">Logo</h1>
+              <p className="font-inter  text-xl">Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-6 w-[200px] items-center">
-            <h1 className="font-inter font-semibold text-2xl text-white">Lien rapide</h1>
-            <Link to="/Signup" className="font-inter text-white text-xl">sign up</Link>
-            <Link to="/Login" className='font-inter text-white text-xl'>log in</Link>
+        <div className="hidden md:block">
+          <div className="flex flex-col gap-4 lg:gap-6 w-[200px] items-center ">
+              <h1 className="font-inter font-semibold text-lg lg:text-2xl">Lien rapide</h1>
+              <Link to="" className="font-inter  text-lg lg:text-xl">Acceuil</Link>
+              <Link to="" className='font-inter  text-lg lg:text-xl'>Article</Link>
+              <Link to="" className='font-inter  text-lg lg:text-xl'>Favoris</Link>
+              <Link to="" className='font-inter text-lg lg:text-xl'>Add new article</Link>
+          </div>
         </div>
-        <div className="flex flex-col gap-8 w-[200px] items-center">
-            <h1 className="font-inter font-semibold text-2xl text-white"> Contactez nous</h1>
-            <p className="font-inter text-white text-xl">dare elbeida alger</p>
-            <p className="underline font-inter text-white text-xl">service@esi.dz</p>
-            <p className="font-inter text-white text-xl">+21-25533-0000</p>
+      
+        <div className="flex flex-col gap-6 lg:gap-8  w-[200px] items-center ">
+            <h1 className="font-inter text-4xl font-bold md:hidden"> PDFinder</h1>
+            <h1 className="font-inter font-semibold text-xl lg:text-2xl"> Contactez nous</h1>
+            <p className="font-inter text-lg lg:text-xl">dare elbeida alger</p>
+            <p className="underline font-inter  text-lg lg:text-xl">service@esi.dz</p>
+            <p className="font-inter  text-lg lg:text-xl">+21-25533-0000</p>
 
         </div>
         
       </div>
-      <div className="flex justify-center pb-4 text-white text-2xl font-inter">
+      <div className="flex justify-center pb-4 text-white text-xl sm:text-2xl font-inter">
          2023 pdf .All rights reserved 
      </div>
     </div>
--- src/components/navbars/NavArticles.jsx
+++ src/components/navbars/NavArticles.jsx
@@ -8,14 +8,18 @@ import FooterSigned from '../footers/FooterSigned';
 export default function NavArticles() {
   return (
     <nav className={`${styles.paddingX} ${styles.paddingY}  flex items-center justify-between bg-[#1B76FF]`}>
-      <Link to="/Home" className="font-jost font-bold text-5xl text-white  ">PDFinder</Link>
-      <div className="flex gap-10 items-center">
-        <div className="flex py-2 px-3 bg-white rounded-full w-[500px] shadow-[0px_5px_10px_rgb(140,140,140)]">
-            <input className=" w-full text-lg font-inter font-semibold outline-none px-4 placeholder-opacity-100 placeholder-black" placeholder="General relativity ... "/>
-            <img src={SearchIcon} alt="" className="cursor-pointer"/>
-        </div> 
-        <p className="font-inter text-white text-2xl cursor-pointer">Filtrer</p>
+      <Link to="/Home" className="font-jost font-bold text-4xl lg:text-5xl text-white  ">PDFinder</Link>
+      <div className="hidden lg:block">
+        <div className="flex gap-10 items-center">
+            <div className="flex py-2 px-3 bg-white rounded-full lg:w-[400px] 2xl:w-[500px] shadow-[0px_5px_10px_rgb(140,140,140)] ">
+                <input className=" w-full text-lg font-inter font-semibold outline-none px-4 placeholder-opacity-100 placeholder-black" placeholder="General relativity ... "/>
+                <img src={SearchIcon} alt="" className="cursor-pointer"/>
+            </div> 
+
+          <p className="font-inter text-white text-2xl cursor-pointer">Filtrer</p>
+        </div>
       </div>
+
       <div className="flex gap-8 items-center">
         <MenuIcon fontSize="large"  />
         <div className="flex justify-center items-center h-[70px] w-[70px] rounded-full bg-[#E1F8FF]">
