3a9e20e0d71bf479019fcbb23a8f8ea92aa3b153
ccd <lm_bouchene@esi.dz>
2024-01-13 03:01:47 -1100

    worked on the edit page's responsiveness Only the responsive search bar remaining

--- src/components/ModEditForm.jsx
+++ src/components/ModEditForm.jsx
@@ -18,26 +18,28 @@ export default function ModEditForm() {
     }
   return (
     <div className="max-w-[600px] mx-auto ">
-        <h1 className="font-inter font-bold text-6xl text-center py-[100px]">Edit Moderator</h1>
+        <h1 className="font-inter font-bold text-3xl xs:text-4xl sm:text-5xl md:text-6xl text-center py-[75px] sm:py-[100px]">Edit Moderator !</h1>
         
         <form onSubmit={handleSubmit} className="px-12 pb-16">
-            <label className="block font-inter text-xl pb-5 font-semibold">name :</label>
+            <label className="block font-inter text-xl sm:text-2xl pb-5 font-semibold">name :</label>
             <div className="w-full">
                 <input type="text" name="name" className="w-full rounded-lg font-inter text-xl px-4 py-3 outline-none " />
             </div>
-            <label className="block font-inter text-xl py-6 font-semibold">Email :</label>
+            <label className="block font-inter text-xl sm:text-2xl py-6 font-semibold">Email :</label>
             <div className="w-full">
                 <input type="email" name="email" className="w-full rounded-lg font-inter text-xl px-4 py-3 outline-none" />
             </div>            
-            <label className="block font-inter text-xl py-6 font-semibold">Phone :</label>
+            <label className="block font-inter text-xl sm:text-2xl py-6 font-semibold">Phone :</label>
             <div className="w-full">
                 <input type="text" name="phone" className="w-full rounded-lg font-inter text-xl px-4 py-3 outline-none"/>
             </div>
-            <label className="block font-inter text-xl py-6 font-semibold">Password :</label>
+            <label className="block font-inter text-xl sm:text-2xl py-6 font-semibold">Password :</label>
             <div className="w-full ">
                 <input type="password" name="password" className="w-full rounded-lg font-inter text-xl px-4 py-3 outline-none"/>
             </div>
-            <button className="w-full my-10 py-3 bg-main text-white font-inter rounded-xl text-2xl">Add Moderator</button>
+            <button className="w-full mt-10 py-3 bg-main text-white font-inter rounded-xl text-xl sm:text-2xl">Edit Moderator</button>
+            <button className="w-full my-10 py-3 bg-red-500 text-white font-inter rounded-xl text-xl sm:text-2xl">Delete Moderator</button>
+
         </form>
     </div>
   )
--- src/pages/AdminPages/Password.jsx
+++ src/pages/AdminPages/Password.jsx
@@ -21,9 +21,9 @@ export default function Password() {
     <form action="" >
         
 
-        <h1 className="text-center font-inter font-semibold text-5xl xl:text-6xl py-[50px] sm:py-[75px] md:py-[100px]">Change your Password</h1>
+        <h1 className="text-center font-inter font-semibold text-3xl sm:text-4xl md:text-5xl xl:text-6xl py-[50px] sm:py-[75px] md:py-[100px]">Change your Password</h1>
 
-      <div className="mt-[70px] bg-white w-[90%] md:max-w-[600px] lg:max-w-[700px] xl:max-w-[800px] 2xl:max-w-[900px] mx-auto rounded-2xl ">
+      <div className="md:mt-[70px] bg-white w-[90%] md:max-w-[600px] lg:max-w-[700px] xl:max-w-[800px] 2xl:max-w-[900px] mx-auto rounded-2xl ">
 
         <div className="flex flex-col sm:flex-row  py-16 ustify-center sm:justify-normal sm:items-center w-full pl-[10%] sm:pl-0 gap-[10px] sm:gap-0">
           <div className="w-full sm:w-[200px] lg:w-[250px]  ml-0 sm:ml-[15%] lg:ml-[20%] ">
@@ -44,6 +44,10 @@ export default function Password() {
             <input type="password" className="border-[#A9AABC] border-2 w-[90%] sm:w-[250px] lg:w-[280px] outline-none py-1 text-center font-inter text-xl text-[#A9AABC]"/>
         </div>
 
+
+      </div>
+      <div className="mt-[70px] flex  justify-center mx-auto">
+          <button type="submit" className="sm:w-[300px] lg:w-[500px] text-2xl lg:text-3xl font-inter font-semibold text-white bg-main py-5 px-10 rounded-xl" >Edit Password</button>
       </div>
     </form>
 
